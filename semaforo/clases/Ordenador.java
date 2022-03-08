5f30af01738882c4f0c0d7dab767900e50c19e75
adrianburgoos <101181480+adrianburgoos@users.noreply.github.com>
2022-03-08 18:00:45 +0100

    Update Ordenador.java

--- semaforo/clases/Ordenador.java
+++ semaforo/clases/Ordenador.java
@@ -1,7 +1,18 @@
 package clases;
 import java.util.Random;
+/**
+ * Recoje todos los datos de las otras clases
+ * @author Adrián Burgos
+ */
 public class Ordenador {
+    /**
+     * Constructor del ordenador.
+     */
     public Ordenador() {}
+    /**
+     * Método que devuelve un color random para cada estudiante
+     * @return Color random
+     */
     public String color(){
     	Random randomGenerator = new Random();
 	int randomInt = randomGenerator.nextInt(3);
