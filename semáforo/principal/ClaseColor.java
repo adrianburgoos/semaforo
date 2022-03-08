427f013b560b667f85a72adb1d636ae1e8b946d2
adrianburgoos <101181480+adrianburgoos@users.noreply.github.com>
2022-03-08 17:55:50 +0100

    Update and rename semaforo/principal/ClaseColor.java to semáforo/principal/ClaseColor.java

--- semáforo/principal/ClaseColor.java
+++ semáforo/principal/ClaseColor.java
@@ -1,6 +1,11 @@
-package principal;
+package package principal;
 import clases.Profesor;
 // Clase color, el profesor pregunta a un alumno por un color entre (rojo, amarillo y verde)
+
+/**
+ * Clase color que se va a usar en las clsaes para poner el color del semaforo
+ * @author Adrián Burgos
+ */
 public class ClaseColor {
 	public static void main(String[] args) {
 		Profesor teacher = new Profesor();
