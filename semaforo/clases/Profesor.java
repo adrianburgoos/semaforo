b3c1ccd203f871f010e2b4ea1a293db7dad9a5f2
adrianburgoos <101181480+adrianburgoos@users.noreply.github.com>
2022-03-08 18:05:08 +0100

    Update Profesor.java

--- semaforo/clases/Profesor.java
+++ semaforo/clases/Profesor.java
@@ -1,9 +1,17 @@
 package clases;
 
+/**
+ * Persona que se encarga de preguntar los colores a los estudiantes
+ * @author Usuario
+ */
 public class Profesor  extends Persona{
     	public Profesor() {}				// Constructor
 
 	// Hace la pregunta al estudiante sobre el color
+        /**
+         * Pregunta al estudiante que color va a elegir.
+         * @return Color que ha elegido el estudiante
+         */
     	public String preguntacolor(){
 
 		Estudiante alumno = new Estudiante();
