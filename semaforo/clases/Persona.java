19978d619ebfb963bed2bc14ef0676b6e314bedc
adrianburgoos <101181480+adrianburgoos@users.noreply.github.com>
2022-03-08 18:04:01 +0100

    Update Persona.java

--- semaforo/clases/Persona.java
+++ semaforo/clases/Persona.java
@@ -1,5 +1,9 @@
 package clases;
 // Clase utilizada para ser herencia de estudiante y profesor
+/**
+ * Creas personas, estudiantes o profesores.
+ * @author Adrián Burgos
+ */
 public class Persona {
 	// Metodos de clase. Edad y nombre
 	int i_Edad;	
