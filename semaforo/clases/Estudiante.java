0290a700e5f31fa03f69e805743784653fee5558
adrianburgoos <101181480+adrianburgoos@users.noreply.github.com>
2022-03-08 17:58:31 +0100

    Update Estudiante.java

--- semaforo/clases/Estudiante.java
+++ semaforo/clases/Estudiante.java
@@ -1,15 +1,29 @@
 package clases;
+/**
+ * Clase que define a estudiante, esta se extiende a Persona.
+ * @author Usuario
+ */
 public class Estudiante extends Persona{
 	// Incluye un metodo de clase que se une a los heredados
 	int i_Curso;
+        /**
+         * Ofrece los datos del estudiante
+         */
     	public Estudiante() {
 		i_Edad=25;
 		s_Nombre = "Luis";
 		i_Curso = 1;
 	}
+        /**
+         * Imprime una pequeña presentación del Estudiante.
+         */
 	public void presentarse(){
 	System.out.println("Soy " + s_Nombre + " Alumno de " + Integer.toString(i_Curso) + " y 	tengo una edad de: " + Integer.toString(i_Edad));	
     	}
+        /**
+         * Método para saber el color que quiere el estudiante, para ello pregunta al ordenador.
+         * @return Devuelve el color del Estudiante sacado del Ordenador.
+         */
 	public String preguntacolor(){
 		presentarse();
 		Ordenador mipc = new Ordenador();
